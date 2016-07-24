e685b568f9d8a99c8c1c038dc7d12abf73d8e9a0
Laura Gardner <elgardner@gmail.com>
2016-07-24 14:46:21 +0100

    Exercises from Basics chapter

---
+++ basics3.rb
@@ -0,0 +1,5 @@
+movies = {:clueless => 1995, :eternal_sunshine_of_the_spotless_mind => 2004, :amelie => 2001, :labyrinth => 1986}
+puts movies[:clueless]
+puts movies[:eternal_sunshine_of_the_spotless_mind]
+puts movies[:amelie]
+puts movies[:labyrinth]
---
+++ basics4.rb
@@ -0,0 +1,5 @@
+moviedates = [1995, 2004, 2001, 1986]
+puts moviedates[0]
+puts moviedates[1]
+puts moviedates[2]
+puts moviedates[3]
